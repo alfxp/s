67ef3cd335e5dddfc81378ee075ee54c508033a1
Alfredo <alfxp@yahoo.com.br>
2022-09-10 21:44:45 -0300

    .

--- libraryScript.sh
+++ libraryScript.sh
@@ -198,7 +198,7 @@ function installVim(){
 function AllowSSH(){
 
     # Vi /etc/ssh/sshd_config
-    sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+    sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 }
 
 
